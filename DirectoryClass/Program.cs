e4357d0b82a71bc9a49ac2860d154416f8990c8a
KamolHasanov <khasanovkamol3834@gmail.com>
2021-06-03 08:52:13 +0500

    Add project files.

---
+++ DirectoryClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+namespace DirectoryClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Birxadli funksiya kiriting");
+            Console.Write("koyefsentini kiting >> ");
+            double koy = double.Parse(Console.ReadLine());
+            
+            Console.Write("argumentini kiting >> ");
+            string argument = Console.ReadLine();
+            
+            Console.Write("darajasini kiting >> ");
+            double daraja = double.Parse(Console.ReadLine());
+            
+            Console.Write("necha marta hosila olasiz >> ");
+            double n = double.Parse(Console.ReadLine());
+
+            KO_PHAD ob = new KO_PHAD();
+            ob.hisobla(koy, argument, daraja, n);
+        }
+    }
+    
+}
